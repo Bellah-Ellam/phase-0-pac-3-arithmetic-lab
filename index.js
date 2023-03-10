90aa0460f438fef9da9a7828cff26fca79e82142
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-03-10 21:43:38 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,53 @@
+number=10
+
+function add() {
+    return (number += 5);
+  }
+  
+function subtract() {
+    return (number -= 5);
+  } 
+  function multiply() {
+    return (number *= 5);
+  } 
+
+
+function divide() {
+    return (number /= 3);
+  } 
+
+  function add(a,b) {
+    return (a+b);
+  }
+  
+  function subtract(a,b) {
+    return (a-b);
+  }
+
+  function multiply(a,b) {
+    return (a*b);
+  }
+
+  function divide(a,b) {
+    return (a/b);
+  }
+
+function increment(a) {
+    return (a+1)
+}
+ 
+
+function decrement(a) {
+    return (a-1)
+}
+ 
+
+function makeInt(a){
+    return parseInt(a,10)
+
+  
+}
+
+function preserveDecimal(a) {
+    return parseFloat(a)
+}
